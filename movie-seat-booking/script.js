6595f37e393d40bd404cf1a7a4b904216f31e84a
matthew paravati <matthew.parvati@oncor.com>
2020-08-15 11:18:20 -0500

    movie seat booking - saves data to localStorage, populates UI with saved data

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -4,7 +4,21 @@ const count = document.querySelector('#count');
 const total = document.querySelector('#total');
 const movieSelect = document.querySelector('#movie');
 
+// Populate UI on page load
+populateUI();
+
 let ticketPrice = parseInt(movieSelect.value);
+
+// Save selected movie
+function saveMovieData(movieIndex) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+}
+
+// Save selected seats
+function saveSelectedSeats(selectedSeats) {
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+}
 
 // Update total and count
 function updatedSelectedCount() {
@@ -13,11 +27,34 @@ function updatedSelectedCount() {
 
   count.textContent = selectedSeatsCount;
   total.textContent = selectedSeatsCount * ticketPrice;
+
+  // const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+  // localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+  saveSelectedSeats(selectedSeats);
+}
+
+// Get data from localStorage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  if ((selectedSeats !== null) & (selectedSeats.length > 0)) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
 }
 
 // Movie select event
 movieSelect.addEventListener('change', e => {
   ticketPrice = parseInt(e.target.value);
+  saveMovieData(e.target.selectedIndex);
   updatedSelectedCount();
 });
 
@@ -32,3 +69,6 @@ container.addEventListener('click', e => {
     updatedSelectedCount();
   }
 });
+
+//  Initial count and total set
+updatedSelectedCount();
