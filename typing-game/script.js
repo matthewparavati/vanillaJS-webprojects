508b522cbb0a9ecdb4ab9c0416fbab3ea69fddf6
matthew paravati <matthew.parvati@oncor.com>
2020-08-16 11:22:40 -0500

    typing game - add functionality to project

---
+++ typing-game/script.js
@@ -0,0 +1,134 @@
+const word = document.querySelector('#word');
+const text = document.querySelector('#text');
+const scoreEl = document.querySelector('#score');
+const timeEl = document.querySelector('#time');
+const endgameEl = document.querySelector('#end-game-container');
+const settingsBtn = document.querySelector('#settings-btn');
+const settings = document.querySelector('#settings');
+const settingsForm = document.querySelector('#settings-form');
+const difficultySelect = document.querySelector('#difficulty');
+
+// List of words for game
+const words = [
+  'sigh',
+  'tense',
+  'airplane',
+  'ball',
+  'pies',
+  'juice',
+  'warlike',
+  'bad',
+  'north',
+  'dependent',
+  'steer',
+  'silver',
+  'highfalutin',
+  'superficial',
+  'quince',
+  'eight',
+  'feeble',
+  'admit',
+  'drag',
+  'loving',
+];
+
+// Init word
+let randomWord;
+
+// Init score
+let score = 0;
+
+// Init time
+let time = 10;
+
+// Init difficulty
+let difficulty = localStorage.getItem('difficulty')
+  ? localStorage.getItem('difficulty')
+  : 'medium';
+
+// Set difficulty select value
+difficultySelect.value = difficulty;
+
+// Focus on text on start
+text.focus();
+
+// Start counting down
+const timeInterval = setInterval(updateTime, 1000);
+
+// Get random word
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)];
+}
+
+// Add word to DOM
+function addWordToDOM() {
+  randomWord = getRandomWord();
+
+  word.innerHTML = randomWord;
+}
+
+// Update score
+function updateScore() {
+  score++;
+  scoreEl.innerHTML = score;
+}
+
+// Update time
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + 's';
+
+  if (time === 0) {
+    clearInterval(timeInterval);
+
+    // End game
+    gameOver();
+  }
+}
+
+// Game over, show end screen
+function gameOver() {
+  endgameEl.innerHTML = `
+    <h1>Time ran out</h1>
+    <p>You final score is ${score}</p>
+    <button onclick="location.reload()">Reload</button>
+  `;
+
+  endgameEl.style.display = 'flex';
+}
+
+addWordToDOM();
+
+// Event listeners
+
+// Typing
+text.addEventListener('input', e => {
+  const insertedText = e.target.value;
+
+  if (insertedText == randomWord) {
+    addWordToDOM();
+    updateScore();
+
+    // Clear input
+    e.target.value = '';
+
+    if (difficulty === 'hard') {
+      time += 2;
+    } else if (difficulty === 'medium') {
+      time += 3;
+    } else {
+      time += 5;
+    }
+
+    updateTime();
+  }
+});
+
+// Settings buttons click
+settingsBtn.addEventListener('click', () => settings.classList.toggle('hide'));
+
+// Settings select
+settingsForm.addEventListener('change', e => {
+  difficulty = e.target.value;
+  localStorage.setItem('difficulty', difficulty);
+});
